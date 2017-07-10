6f2f33eb12496c22d0dbfe8150b934d3004f8301
jieming123 <zhouxin@zhouxindeMacBook-Pro.local>
2017-07-10 17:49:13 +0800

    Initial Commit

---
+++ Swift_4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift_4
+//
+//  Created by 周鑫 on 2017/7/10.
+//  Copyright © 2017年 周鑫. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
